2b1b8a13a4f0a4ddfee087ccfd16d00b0d72c76d
ColonyVendorBond <redickvi833@gmail.com>
2026-06-12 19:44:34 -0600

    Create LifeProgressWidget/YearProgress/YearCalendarView.swift

---
+++ LifeProgressWidget/YearProgress/YearCalendarView.swift
@@ -0,0 +1,56 @@
+import SwiftUI
+
+struct YearCalendarView: View {
+    var life: Life
+
+    var body: some View {
+        Canvas { context, size in
+            let numberOfColumns = 8
+            let numberOfRows = Life.numberOfWeeksInAYear / numberOfColumns + 1
+            let cellWidth = size.width / Double(numberOfColumns)
+            let cellHeight = size.height / Double(numberOfRows)
+            let cellPadding = cellWidth / 32
+
+            let scaleFactor = size.width / (size.width - cellPadding * 2)
+            let scaleTransform = CGAffineTransform(
+                scaleX: scaleFactor,
+                y: scaleFactor
+            )
+            let translateTransform = CGAffineTransform(
+                translationX: -cellPadding * scaleFactor,
+                y: -cellPadding * scaleFactor
+            )
+
+            context.concatenate(scaleTransform)
+            context.concatenate(translateTransform)
+
+            for weekIndex in 0 ..< Life.numberOfWeeksInAYear {
+                let rowIndex = weekIndex / numberOfColumns
+                let columnIndex = weekIndex % numberOfColumns
+
+                let cellPath =
+                    Path(
+                        CGRect(
+                            x: Double(columnIndex) * cellWidth + cellPadding,
+                            y: Double(rowIndex) * cellHeight + cellPadding,
+                            width: cellWidth - cellPadding * 2,
+                            height: cellHeight - cellPadding * 2
+                        )
+                    )
+
+                let fillColor = weekIndex < life.currentYearNumberOfWeeksSpent
+                    ? AgeGroup(age: life.age + 1).getColor()
+                    : Color(uiColor: .systemFill)
+
+                context.fill(cellPath, with: .color(fillColor))
+            }
+        }
+    }
+}
+
+struct YearCalendarView_Previews: PreviewProvider {
+    static var previews: some View {
+        YearCalendarView(life: Life.example)
+            .previewLayout(.fixed(width: 300, height: 200))
+    }
+}
