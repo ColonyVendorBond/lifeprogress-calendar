e7000b03d3e3b71bce532cd584e48597d55997cb
ColonyVendorBond <redickvi833@gmail.com>
2026-06-12 19:44:24 -0600

    Create LifeProgressWidget/LifeProgress/LifeProgressWidget.swift

---
+++ LifeProgressWidget/LifeProgress/LifeProgressWidget.swift
@@ -0,0 +1,30 @@
+import SwiftUI
+import WidgetKit
+
+struct LifeProgressWidget: Widget {
+    let kind: String = "LifeProgressWidget"
+
+    var body: some WidgetConfiguration {
+        StaticConfiguration(kind: kind, provider: Provider()) { _ in
+            LifeProgressWidgetEntryView()
+        }
+        .configurationDisplayName("Life Progress")
+        .description("Friendly reminder that you're not gonna live forever.")
+        .supportedFamilies(supportedFamilies)
+        .contentMarginsDisabled()
+    }
+
+    private var supportedFamilies: [WidgetFamily] {
+        var widgetFamilies: [WidgetFamily] = [
+            .systemMedium,
+            .systemSmall,
+            .systemLarge,
+        ]
+
+        if #available(iOSApplicationExtension 16.0, *) {
+            widgetFamilies.append(.accessoryCircular)
+        }
+
+        return widgetFamilies
+    }
+}
